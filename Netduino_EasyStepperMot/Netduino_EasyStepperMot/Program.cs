eaaea3e512b7aa72d1b5a9f2c0f2e03e6ea32fa6
Philippe <philippemariano@hotmail.fr>
2014-06-09 15:19:23 +0200

    Revision 1

--- Netduino_EasyStepperMot/Netduino_EasyStepperMot/Program.cs
+++ Netduino_EasyStepperMot/Netduino_EasyStepperMot/Program.cs
@@ -11,35 +11,35 @@ namespace Netduino_EasyStepperMot
     {
         public static void Main()
         {   // Programme de test d'un moteur pas à pas ITC-VNC-1 http://astrojbm.free.fr/bricolages/picastro/Mecanique/ITC_CNC_1_FR.pdf
-            // avec une carte EasyStepperMotor
-            // N=200 pas - U=12V - C=200g/cm
-            var time = 2000;
-            var stepper = new EasyStepperDriver(Pins.GPIO_PIN_D8, Pins.GPIO_PIN_D9, Pins.GPIO_PIN_D10, Pins.GPIO_PIN_D11, Pins.GPIO_PIN_D12);
+            // N=200 pas - U=12V - C=200g/cmavec une carte EasyStepperMotor 
+            var time = 2000; UInt16 delay = 2; UInt32 nbpas = 200;
+            var stepper = new EasyStepperDriver(Pins.GPIO_PIN_D8, Pins.GPIO_PIN_D9, Pins.GPIO_PIN_D10, Pins.GPIO_PIN_D11, Pins.GPIO_PIN_D12,Pins.GPIO_PIN_D13);
 
+            stepper.WakeUp(); 
             while (true)
-            {   // Exemples d'utilisation de la méthode Step() et des propriétés StepMode, StepDirection et StepDelay
-                stepper.Sleep(false);
-                Debug.Print("Full Forward");
-                stepper.GoStep(200,EasyStepperDriver.Mode.Full,EasyStepperDriver.Direction.Forward); // 360° pour le moteur ITC-VNC-1
-                Debug.Print("Pas= " + stepper.Steps + " Mode= " + stepper.StepMode + " Dir= " + stepper.StepDirection + " delay= " + stepper.StepDelay + "ms" + "\n");
-                Thread.Sleep(time);
+            {   // Exemples d'utilisation de la méthode Step() et des propriétés StepMode, StepDirection et Steptime
+                stepper.EnableOutputs(); // Activation des sorties
+                Debug.Print("Sleep= " + stepper.IsDriverSleep + " Enable= " + stepper.IsOutputsEnable);
+                Debug.Print("Full Forward"); // 360° pour le moteur ITC-VNC-1
+                stepper.Turn(nbpas, EasyStepperDriver.Direction.Forward, delay,EasyStepperDriver.Mode.Full); 
+                Debug.Print("Pas= " + stepper.Steps + " Mode= " + stepper.StepMode + " Dir= " + stepper.StepDirection + " time= " + stepper.StepDelay + "ms" + "\n");
+                stepper.DisableOutputs(); Thread.Sleep(time); // Désactivation des sorties pendant la temporisation
 
-                Debug.Print("Half Backward");
-                stepper.GoStep(200, EasyStepperDriver.Mode.Half,EasyStepperDriver.Direction.Backward); // 180° pour le moteur ITC-VNC-1
-                Debug.Print("Pas= " + stepper.Steps + " Mode= " + stepper.StepMode + " Dir= " + stepper.StepDirection + " delay= " + stepper.StepDelay + "ms" + "\n");
-                Thread.Sleep(time);
+                Debug.Print("Half Backward"); stepper.EnableOutputs(); // 180° pour le moteur ITC-VNC-1
+                stepper.Turn(nbpas, EasyStepperDriver.Direction.Backward, delay, EasyStepperDriver.Mode.Half); 
+                Debug.Print("Pas= " + stepper.Steps + " Mode= " + stepper.StepMode + " Dir= " + stepper.StepDirection + " time= " + stepper.StepDelay + "ms" + "\n");
+                stepper.DisableOutputs(); Thread.Sleep(time); // Désactivation des sorties pendant la temporisation
 
-                Debug.Print("Quater Forward"); // 90° pour le moteur ITC-VNC-1
-                stepper.GoStep(200, EasyStepperDriver.Mode.Quarter, EasyStepperDriver.Direction.Forward);
-                Debug.Print("Pas= " + stepper.Steps + " Mode= " + stepper.StepMode + " Dir= " + stepper.StepDirection + " delay= " + stepper.StepDelay + "ms" + "\n");
-                Thread.Sleep(time);
+                Debug.Print("Quater Forward"); stepper.EnableOutputs();  // 90° pour le moteur ITC-VNC-1
+                stepper.Turn(nbpas, EasyStepperDriver.Direction.Forward, delay, EasyStepperDriver.Mode.Quarter);
+                Debug.Print("Pas= " + stepper.Steps + " Mode= " + stepper.StepMode + " Dir= " + stepper.StepDirection + " time= " + stepper.StepDelay + "ms" + "\n");
+                stepper.DisableOutputs(); Thread.Sleep(time); // Désactivation des sorties pendant la temporisation
 
-                Debug.Print("OneEighth Backward"); // 45° pour le moteur ITC-VNC-1
-                stepper.GoStep(200,EasyStepperDriver.Mode.OneEighth,EasyStepperDriver.Direction.Backward,1);
-                Debug.Print("Pas= " + stepper.Steps + " Mode= " + stepper.StepMode + " Dir= " + stepper.StepDirection + " delay= " + stepper.StepDelay + "ms" + "\n");
-                
-                stepper.Sleep(true);
-                Thread.Sleep(5*time);
+                Debug.Print("OneEighth Backward"); stepper.EnableOutputs(); // 45° pour le moteur ITC-VNC-1
+                stepper.Turn(nbpas, EasyStepperDriver.Direction.Backward, 1, EasyStepperDriver.Mode.OneEighth);
+                Debug.Print("Pas= " + stepper.Steps + " Mode= " + stepper.StepMode + " Dir= " + stepper.StepDirection + " time= " + stepper.StepDelay + "ms" + "\n");
+                stepper.DisableOutputs(); Thread.Sleep(time); // Désactivation des sorties pendant la temporisation
+                Thread.Sleep(2 * time);
             }
         }
     }
